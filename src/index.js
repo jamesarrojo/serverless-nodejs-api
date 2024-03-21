a591ea70974da47e22a96a15abf7aa3d8539af33
jamesarrojo.01 <jamesarrojo.01@gmail.com>
2024-03-21 14:46:24 +0800

    done with Decouple and Upgrade AWS SSM Client

--- src/index.js
+++ src/index.js
@@ -1,37 +1,16 @@
 const serverless = require('serverless-http');
 const express = require('express');
-const AWS = require('aws-sdk');
-const { neon, neonConfig } = require('@neondatabase/serverless'); //commonjs
-
-const AWS_REGION = 'ap-southeast-1';
-const STAGE = process.env.STAGE || 'prod';
-
+const { getDbClient } = require('./db/clients');
 const app = express();
-const ssm = new AWS.SSM({ region: AWS_REGION }); //initialize SSM connection
-
-const DATABASE_URL_SSM_PARAM = `/serverless-nodejs-api/${STAGE}/database-url`;
-
-async function dbClient() {
-    // for http connections
-    // non-pooling
-    const paramStoreData = await ssm
-        .getParameter({
-            Name: DATABASE_URL_SSM_PARAM,
-            WithDecryption: true, //because we stored it as SecureString
-        })
-        .promise();
-    // const dbUrl = ??
-    neonConfig.fetchConnectionCache = true;
-    const sql = neon(paramStoreData.Parameter.Value);
-    return sql;
-}
 
 app.get('/', async (req, res, next) => {
-    const sql = await dbClient();
-    const [results] = await sql`select now();`;
+    const sql = await getDbClient();
+    const now = Date.now();
+    const [dbNowResult] = await sql`select now();`;
+    const delta = (dbNowResult.now.getTime() - now) / 1000;
     return res.status(200).json({
         message: 'Hello from root!',
-        results: results.now,
+        delta: delta,
     });
 });
 
